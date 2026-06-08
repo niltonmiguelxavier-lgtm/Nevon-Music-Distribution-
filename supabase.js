db2878e29e1dba1483c4a741a9d71e422a837acc
niltonmiguelxavier-lgtm <niltonmiguelxavier@gmail.com>
2026-06-08 06:40:51 +0200

    Delete supabase.js

--- supabase.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
-
-const SUPABASE_URL = "https://ajhpvuoyhviyjfkpctxg.supabase.co";
-
-const SUPABASE_ANON_KEY = "COLOCA_AQUI_A_TUA_KEY";
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
